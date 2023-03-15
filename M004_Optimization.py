1729ada74057eef614307daba9318120cd888949
Barbu <bbsoft0@gmail.com>
2023-03-15 21:26:15 +0200

    Added requirements.txt and general updates

--- M004_Optimization.py
+++ M004_Optimization.py
@@ -39,7 +39,7 @@ x, y = meshgrid(xaxis, yaxis)
 results = objective(x, y)
 # create a surface plot with the jet color scheme
 figure = pyplot.figure()
-axis = figure.gca(projection='3d')
+axis = figure.add_subplot(projection='3d')
 axis.plot_surface(x, y, results, cmap='jet')
 # show the plot
 pyplot.show()
@@ -67,7 +67,7 @@ x, y = meshgrid(xaxis, yaxis)
 results = objective(x, y)
 # create a surface plot with the jet color scheme
 figure = pyplot.figure()
-axis = figure.gca(projection='3d')
+axis = figure.add_subplot(projection='3d')
 axis.plot_surface(x, y, results, cmap='jet')
 # show the plot
 pyplot.show()
